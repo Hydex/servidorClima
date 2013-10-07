1f6772554f4441f57aa13cf9b30032141abf73ce
Edwin Cornejo <ed.cornejo89@gmail.com>
2013-10-06 22:32:49 -0500

    version 1 sacando la temperatura arequipa

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -12,13 +12,13 @@ public class HelloWorld extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-      String inputText="";
+     
                 ////////
 
             try {
                   // Indicamos la URL donde nos conectamos
+//                  URL url = new URL("http://weather.yahooapis.com/forecastrss?w=418442");
                   URL url = new URL("http://weather.yahooapis.com/forecastrss?w=418442");
-
                   // Buffer con los datos recibidos
                   BufferedReader in = null;
                
@@ -31,7 +31,7 @@ public class HelloWorld extends HttpServlet {
 
                   // Transformamos el contenido del buffer a texto
                   String inputLine;
-                 // String inputText="";
+                  String inputText="";
 
                   // Mientras haya cosas en el buffer las volcamos a las
                   // cadenas de texto 
@@ -41,7 +41,19 @@ public class HelloWorld extends HttpServlet {
                   }
 
                   // Mostramos el contenido y cerramos la entrada
-                 // System.out.println("El contenido de la URL es: " + inputText);
+                  //System.out.println("\n\nEl contenido de la URL es: \n\n" + inputText);
+                  
+                  //System.out.println(inputText.indexOf("temp=\""));
+                  String temp = inputText.substring(inputText.indexOf("temp=\""), inputText.indexOf("temp=\"")+10);
+                 // System.out.println("Temperatura en Arequipa"+temp);
+                
+
+
+                  String example="<!DOCTYPE html><html><body><form action=\"demo_form.asp\" method=\"get\"><input list=\"browsers\" name=\"browser\"><datalist id=\"browsers\"><option value=\"Internet Explorer\"><option value=\"Firefox\"><option value=\"Chrome\"><option value=\"Opera\"><option value=\"Safari\"></datalist><input type=\"submit\"></form><p><b>Temperatura en Arequipa "+ temp+"</p></body></html>";
+
+                  //"<!DOCTYPE HTML><html><head><style type=\"text/css\">#div1 {width:350px;height:70px;padding:10px;border:1px solid #aaaaaa;}</style><script>function allowDrop(ev){ev.preventDefault();}function drag(ev){ev.dataTransfer.setData(\"Text\",ev.target.id);}function drop(ev){ev.preventDefault();var data=ev.dataTransfer.getData(\"Text\");ev.target.appendChild(document.getElementById(data));}</script></head><body><p>Drag the W3Schools image into the rectangle:</p><div id=\"div1\" ondrop=\"drop(event)\" ondragover=\"allowDrop(event)\"></div><br><img id=\"drag1\" src=\"img_logo.gif\" draggable=\"true\" ondragstart=\"drag(event)\" width=\"336\" height=\"69\"></body></html>";
+
+                  resp.getWriter().print(example);
                  // resp.getWriter().print(inputText);
                   in.close();
                
@@ -55,7 +67,7 @@ public class HelloWorld extends HttpServlet {
 
 
 
-        resp.getWriter().print("Ingenieria de Software 2 2 is \n UNSA!\n"+inputText);
+       // resp.getWriter().print("Ingenieria de Software 2 2 is \n UNSA!\n"+inputText);
     }
 
     public static void main(String[] args) throws Exception{
