de246307bc402556e553b6908c2b57e09a4a77e0
Edwin Cornejo <ed.cornejo89@gmail.com>
2013-10-29 14:06:41 -0500

    Revision2

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -7,13 +7,68 @@ import org.eclipse.jetty.servlet.*;
 import java.net.*;
 import java.io.*;
 ////////////////////
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+///////////////////
 public class HelloWorld extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
      
+        Connection con = null;
+        Statement st = null;
+        ResultSet rs = null;
+
+      //  String url = "jdbc:mysql://localhost:3306/testdb";
+        String url =  "jdbc:mysql://addons-sso.heroku.com/apps/isw2unsa2013/addons/cleardb:ignite"
+        String user = "b2cd9b7818f84e";
+        String password = "8cd6dee1";
+
+        try {
+            con = DriverManager.getConnection(url, user, password);
+            st = con.createStatement();
+            rs = st.executeQuery("SELECT VERSION()");
+
+            if (rs.next()) {
+                System.out.println(rs.getString(1));
+            }
+
+        } catch (SQLException ex) {
+            Logger lgr = Logger.getLogger(Version.class.getName());
+            lgr.log(Level.SEVERE, ex.getMessage(), ex);
+
+        } finally {
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+                if (st != null) {
+                    st.close();
+                }
+                if (con != null) {
+                    con.close();
+                }
+
+            } catch (SQLException ex) {
+                Logger lgr = Logger.getLogger(Version.class.getName());
+                lgr.log(Level.WARNING, ex.getMessage(), ex);
+            }
+        }
+
+
+
+
+
                 ////////
+
+
 
             try {
                   // Indicamos la URL donde nos conectamos
